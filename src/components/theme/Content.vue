129bd2614d8ef317e96ed603a48379c799662b67
SliverRiver327 <Restent@tutanota.com>
2023-12-25 21:32:38 +0800

    Rewrite introduction

--- src/components/theme/Content.vue
+++ src/components/theme/Content.vue
@@ -1,38 +1,88 @@
 <template>
-    <v-container style="max-width:770px;">
+    <v-container style="max-width:900px;">
         <v-card class="mx-auto my-4 w-full">
 
             <v-img lazy-src="https://library.gxres.net/images/introBanner.webp"
-                src="https://library.gxres.net/images/introBanner.webp" height="410px" cover
+                src="https://library.gxres.net/images/introBanner.webp" :height="height" cover
                 alt="Introduction Banner"></v-img>
 
             <v-card-text>
-                <h5 class="text-h5">Hi there</h5>
-                <p class="text-body-1">Here is Restent Ou, with his partner SliverRiver.</p>
-                <h5 class="text-h5">My careers (?)</h5>
-                <ul>
-                    <li class="text-body-1">Student in Guangxi, China</li>
-                    <li class="text-body-1"><a href="https://github.com/s-complex">Sliver Complex</a> · Main Manager</li>
-                    <li class="text-body-1"><a href="https://github.com/LittleSkinCommspt">LittleSkin Community</a> · Member
-                    </li>
-                    <li class="text-body-1"><a href="https://go.14cloud.cn">14Cloud</a> · Contributor</li>
-                </ul>
-                <h5 class="text-h5">Misc</h5>
-                <ul>
-                    <li class="text-body-1">I like to build front-end website with Vue, main targets are my websites.</li>
-                    <li class="text-body-1">I am interested to IT and electronics. Now I have some experience, and some
-                        ability for fixing them.</li>
-                    <li class="text-body-1">I am a player of Minecraft and rhythm games. But uh, most of my gaming time is
-                        spent on rhythm games.</li>
-                    <li class="text-body-1">I am also a photographer, but my ability is <strong>very BAD</strong>. Main
-                        photos that my websites use were shot by me.</li>
-                </ul>
-                <v-divider />
-                <p class="text-body-1">Ready for next? Open drawer, and explore my websites or contact me!</p>
+                <v-row>
+                    <v-col cols="12" sm="5">
+                        <h5 class="text-h5">Self-introduction</h5>
+                    </v-col>
+                    <v-col cols="12" sm="7">
+                        <p class="text-body-1">Senior high student who comes from Guangxi, China.</p>
+                        <p class="text-body-1">Named Restent Ou, possibly a.k.a. SliverRiver.</p>
+                        <p class="text-body-1">Member of <a href="https://github.com/s-complex">Sliver Complex</a>, <a
+                                href="https://github.com/LittleSkinCommspt">LittleSkin Community</a> and <a
+                                href="https://go.14cloud.cn">14Cloud</a></p>
+                        <p class="text-body-1">Real salted fish in reality.</p>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" sm="5">
+                        <h5 class="text-h5">My skills</h5>
+                    </v-col>
+                    <v-col cols="12" sm="7">
+                        <ul>
+                            <li class="text-body-1">
+                                Build static-site with <a href="https://vuejs.org">Vue</a> (and...<a
+                                    href="https://react.dev">React</a>
+                                sometimes?)
+                            </li>
+                            <li class="text-body-1">PC Building & Reparing (maybe)</li>
+                            <li class="text-body-1">Customer support service</li>
+                            <li class="text-body-1">Basic server maintaince</li>
+                        </ul>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" sm="5">
+                        <h5 class="text-h5">My favorite</h5>
+                    </v-col>
+                    <v-col cols="12" sm="7">
+                        <ul>
+                            <li class="text-body-1">
+                                Play Minecraft and rhythm games
+                            </li>
+                            <li class="text-body-1">Listen to music with types of rhythm, classic chinese, china-pop and
+                                more</li>
+                            <li class="text-body-1">Watch video with types of IT, cooking, etc.</li>
+                        </ul>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" sm="5">
+                        <h5 class="text-h5">Find me</h5>
+                    </v-col>
+                    <v-col cols="12" sm="7">
+                        <p class="text-body-1">Get the way to find me by reading this <a
+                                href="https://library.gxres.net">list</a>.</p>
+                    </v-col>
+                </v-row>
             </v-card-text>
         </v-card>
     </v-container>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const { name } = useDisplay()
+
+const height = computed(() => {
+    // name is reactive and
+    // must use .value
+    switch (name.value) {
+        case 'sm': return 200
+        case 'md': return 500
+    }
+
+    return undefined
+})
+</script>
   
 <script lang="ts">
 export default {
@@ -56,9 +106,12 @@ hr {
 
 ul,
 li {
-    margin: 0.8rem 1.3rem;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 0.7rem;
 }
 
 code {
     font-family: "Jetbrains Mono";
-}</style>
+}
+</style>
--- src/vuetify.ts
+++ src/vuetify.ts
@@ -10,6 +10,16 @@ const vuetify = createVuetify({
       mdi,
     },
   },
+  display: {
+    mobileBreakpoint: 'sm',
+    thresholds: {
+      xs: 0,
+      sm: 340,
+      md: 540,
+      lg: 800,
+      xl: 1280,
+    },
+  },
   theme: {
     themes: {
       light: {
