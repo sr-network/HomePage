7d6afea1e8ea92eb9c4adb6c19416102ca8acbbc
SliverRiver327 <Restent@tutanota.com>
2024-01-03 18:05:42 +0800

    Add auto theme back

--- src/app.vue
+++ src/app.vue
@@ -2,6 +2,22 @@
 import Header from './components/theme/Header.vue';
 import Content from './components/theme/Content.vue';
 import Footer from './components/theme/Footer.vue';
+import { useTheme } from 'vuetify';
+import { onMounted } from 'vue';
+
+const vuetifyTheme = useTheme()
+
+function toggleTheme(themeName?: string) {
+  vuetifyTheme.global.name.value = themeName ?? (vuetifyTheme.global.current.value.dark ? 'light' : 'dark')
+}
+
+onMounted(() => {
+  const darkMatch = matchMedia('(prefers-color-scheme: dark)')
+  toggleTheme(darkMatch.matches ? 'dark' : 'light')
+  darkMatch.addEventListener('change', (event) =>
+    toggleTheme(event.matches ? 'dark' : 'light')
+  )
+})
 </script>
 
 <template>
