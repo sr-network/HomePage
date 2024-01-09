36b3742be29b23e0e2f652c245d84c2e326fcd4d
SliverRiver327 <Restent@tutanota.com>
2024-01-09 18:48:16 +0800

    be simple #2

--- src/components/theme/Content.vue
+++ src/components/theme/Content.vue
@@ -15,12 +15,14 @@ function toggleTheme() {
         <v-container>
             <div class="d-flex justify-center">
                 <div class="pa-2 ma-2"> <v-avatar color="grey" size="130" rounded="1">
-                        <v-img cover src="https://library.gxres.net/images/icons/avatar.webp"></v-img>
+                        <v-img cover lazy-src="https://library.gxres.net/images/icons/avatar.webp"
+                            src="https://library.gxres.net/images/icons/avatar.webp"></v-img>
                     </v-avatar>
                     <v-list-item class="text-center" title="Restent Ou" subtitle="@gxres042"></v-list-item>
                 </div>
                 <div class="pa-2 ma-2"> <v-avatar color="grey" size="130" rounded="1">
-                        <v-img cover src="https://library.gxres.net/images/icons/SliverRiver.webp"></v-img>
+                        <v-img cover lazy-src="https://library.gxres.net/images/icons/SliverRiver.webp"
+                            src="https://library.gxres.net/images/icons/SliverRiver.webp"></v-img>
                     </v-avatar>
                     <v-list-item class="text-center" title="SliverRiver" subtitle="@slivermoe"></v-list-item>
                 </div>
@@ -32,22 +34,27 @@ function toggleTheme() {
             <p>我是 Restent Ou，有时会以 SliverRiver 的名义活动。</p>
             <v-divider />
             <h2>Sites</h2>
-            <div class="d-flex justify-center">
-                <div class="pa-2 ma-2"><v-btn prepend-icon="book" size="large" href="https://blog.gxres.net">BLOG</v-btn>
-                </div>
-                <div class="pa-2 ma-2"><v-btn prepend-icon="library_books" size="large"
-                        href="https://library.gxres.net">LIBRARY</v-btn></div>
-                <div class="pa-2 ma-2"><v-btn prepend-icon="analytics" size="large"
-                        href="https://status.slirv.vip">STATUS</v-btn></div>
-            </div>
+            <v-container>
+                <v-row>
+                    <v-col cols="4"><v-btn prepend-icon="book" size="large"
+                            href="https://blog.gxres.net" stacked>BLOG</v-btn></v-col>
+                    <v-col cols="4"><v-btn prepend-icon="library_books" size="large"
+                            href="https://library.gxres.net" stacked>LIBRARY</v-btn></v-col>
+                    <v-col cols="4"><v-btn prepend-icon="analytics" size="large"
+                            href="https://status.slirv.vip" stacked>STATUS</v-btn></v-col>
+                </v-row>
+            </v-container>
             <h2>Misc</h2>
-            <div class="d-flex justify-center">
-                <div class="pa-2 ma-2"><v-btn prepend-icon="person" size="large"
-                        href="https://library.gxres.net/about">ABOUT (wip)</v-btn></div>
-                <div class="pa-2 ma-2"><v-btn prepend-icon="contacts" size="large"
-                        href="https://library.gxres.net/contact">CONTACT</v-btn></div>
-                <div class="pa-2 ma-2"><v-btn prepend-icon="link" size="large"
-                        href="https://library.gxres.net">FRIENDS</v-btn></div>
+            <v-container>
+                <v-row>
+                    <v-col cols="4"><v-btn prepend-icon="person" size="large"
+                            href="https://library.gxres.net/about" stacked>ABOUT</v-btn></v-col>
+                    <v-col cols="4"><v-btn prepend-icon="contacts" size="large"
+                            href="https://library.gxres.net/contact" stacked>CONTACT</v-btn></v-col>
+                    <v-col cols="4"><v-btn prepend-icon="link" size="large"
+                            href="https://library.gxres.net" stacked>FRIENDS</v-btn></v-col>
+                </v-row>
+            </v-container>
         </div>
-    </div>
-</v-container></template>
+    </v-container>
+</template>
