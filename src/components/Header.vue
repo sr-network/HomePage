00616cb252723d82e2cf12842caaa92689c7ce69
SliverRiver327 <Restent@tutanota.com>
2024-01-15 21:13:22 +0800

    Use @mdui/icons instead of material-icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,36 +3,54 @@
         <mdui-navigation-drawer close-on-overlay-click close-on-esc modal ref="navigationDrawer">
             <mdui-list>
                 <mdui-list-subheader>My sites</mdui-list-subheader>
-                <mdui-list-item icon="person" active>Intro</mdui-list-item>
-                <mdui-list-item icon="book" href="https://blog.gxres.net">Blog</mdui-list-item>
-                <mdui-list-item icon="library_books" href="https://library.gxres.net">Library</mdui-list-item>
+                <mdui-list-item active>
+                    Intro
+                    <mdui-icon-person slot="icon" />
+                </mdui-list-item>
+                <mdui-list-item href="https://blog.gxres.net">
+                    Blog
+                    <mdui-icon-book slot="icon" />
+                </mdui-list-item>
+                <mdui-list-item href="https://library.gxres.net">
+                    Library
+                    <mdui-icon-library-books slot="icon" />
+                </mdui-list-item>
             </mdui-list>
             <mdui-list>
                 <mdui-list-subheader>Quick Access</mdui-list-subheader>
-                <mdui-list-item icon="contacts" href="https://library.gxres.net/contact">Contact</mdui-list-item>
-                <mdui-list-item icon="links" href="https://library.gxres.net/links">Friends</mdui-list-item>
-                <mdui-list-item icon="analytics" href="https://status.slirv.vip">Site Status</mdui-list-item>
+                <mdui-list-item href="https://library.gxres.net/contact">
+                    Contact
+                    <mdui-icon-contact-page slot="icon" />
+                </mdui-list-item>
+                <mdui-list-item href="https://library.gxres.net/links">
+                    Friends
+                    <mdui-icon-link slot="icon" />
+                </mdui-list-item>
+                <mdui-list-item href="https://status.slirv.vip">
+                    Site Status
+                    <mdui-icon-analytics slot="icon" />
+                </mdui-list-item>
             </mdui-list>
         </mdui-navigation-drawer>
 
         <mdui-top-app-bar scroll-behavior="elevate">
-            <mdui-button-icon ref="openButton" icon="menu"></mdui-button-icon>
+            <mdui-button-icon ref="openButton"><mdui-icon-menu /></mdui-button-icon>
             <mdui-top-app-bar-title>Ou's Intro</mdui-top-app-bar-title>
             <div style="flex-grow: 1"></div>
             <mdui-dropdown>
-                <mdui-button-icon slot="trigger" icon="contrast"></mdui-button-icon>
+                <mdui-button-icon slot="trigger"><mdui-icon-contrast /></mdui-button-icon>
                 <mdui-menu>
                     <mdui-menu-item ref="lightMode" value="light">
                         Keep Light
-                        <mdui-icon slot="icon" name="wb_sunny"></mdui-icon>
+                        <mdui-icon-wb-sunny slot="icon" />
                     </mdui-menu-item>
                     <mdui-menu-item ref="darkMode" value="dark">
                         Keep Dark
-                        <mdui-icon slot="icon" name="nightlight"></mdui-icon>
+                        <mdui-icon-nightlight slot="icon" />
                     </mdui-menu-item>
                     <mdui-menu-item ref="autoMode" value="auto">
                         Follow System Settings
-                        <mdui-icon slot="icon" name="computer"></mdui-icon>
+                        <mdui-icon-computer slot="icon" />
                     </mdui-menu-item>
                 </mdui-menu>
             </mdui-dropdown>
@@ -44,6 +62,18 @@
 import { setTheme } from 'mdui';
 import { ref, onMounted } from 'vue';
 import { useThemeStore } from '../stores/themeMode';
+
+import '@mdui/icons/person.js';
+import '@mdui/icons/book.js';
+import '@mdui/icons/library-books.js';
+import '@mdui/icons/contact-page.js';
+import '@mdui/icons/link.js';
+import '@mdui/icons/analytics.js';
+import '@mdui/icons/menu.js';
+import '@mdui/icons/contrast.js';
+import '@mdui/icons/wb-sunny.js';
+import '@mdui/icons/nightlight.js';
+import '@mdui/icons/computer.js';
 
 const store: any = useThemeStore();
 const navigationDrawer = ref<HTMLElement | null>(null);
