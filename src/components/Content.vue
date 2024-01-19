a4a5ddba3949e681275c9cbfa9794145667713af
SliverRiver327 <Restent@tutanota.com>
2024-01-19 13:57:52 +0800

    Rewrite introduciton

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,24 +2,23 @@
     <div class="grid justify-items-center">
         <div class="p-8 max-w-screen-md mdui-prose">
             <h1>👋🏼</h1>
-            <h3>I am Restent Ou, sometimes a.k.a. SliverRiver.
+            <h2>I am Restent Ou, aka SliverRiver.
                 <br />
-                <small>Student · Maintainer · Blogger</small>
-            </h3>
+                <small>Student · Front-end Maintainer · Blogger</small>
+            </h2>
             <hr />
             <h3>I am</h3>
             <ul>
-                <li><a href="https://github.com/s-complex">@s-complex</a>'s owner, as my lab.</li>
-                <li><a href="https://github.com/LittleSkinCommspt">@LittleSkinCommspt</a>'s member.</li>
-                <li><a href="https://go.14cloud.cn">14Cloud</a>'s contributor.</li>
+                <li>Owner of <a href="https://github.com/s-complex">Sliver Complex</a></li>
+                <li>Member of <a href="https://github.com/LittleSkinCommspt">LittleSkin Community</a></li>
+                <li>Contributor of <a href="https://go.14cloud.cn">14Cloud</a></li>
             </ul>
-            <h3>Self introduce</h3>
-            <p>I am just a very very very normal student who comes from Guangxi, China, who wishes to be a trash on the Internet.</p>
-            <p>I don't have any special skills, everything I can do is the basic things that others can learn and do.</p>
-            <p>I am also a fan of Minecraft and rhythm games (such as Phigros and Arcaea).</p>
-            <p>If you are Chinese, reading <a href="https://library.gxres.net/about/me">this version</a> about me may be a good choice.</p>
-            <h3>Find me</h3>
-            <p>Read this <a href="https://library.gxres.net/contact">list</a>.</p>
+            <h3>About me</h3>
+            <p>Just like you see, I am a very very very normal student who comes from Guangxi, China and does everything driven by interest.</p>
+            <p>I started accessing IT and electronics at the age of 8, which droves me to study more knowledge and skills
+                about them. But till now, I don't have any special skills yet, everything I can do is the basic things that
+                others can learn and do.</p>
+            <p>Outside of this, I am a fan of Minecraft and rhythm games (such as Phigros and Arcaea). I am also listening to music, watching videos and eating delicious food in my spare time.</p>
         </div>
     </div>
 </template>
