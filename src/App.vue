dec92cb87feca06e2d498eac7ec101c4d0678cbf
gxres042 <restent@tutanota.com>
2024-04-22 20:21:38 +0800

    Use bun
    New style
    
    Migrate to Vercel

--- src/App.vue
+++ src/App.vue
@@ -3,119 +3,98 @@ const profileStore = useProfileStore();
 
 const toggleContent = () => {
   profileStore.toggleContent();
-}
+};
 
 useHead({
   meta: [
-    { name: "description", content: `Introduction of ${profileStore.name}` }
+    { name: "description", content: `Introduction of ${profileStore.name}` },
   ],
   htmlAttrs: {
-    lang: "zh-CN"
+    lang: "zh-CN",
   },
   link: [
     {
-      rel: 'canonical',
-      href: 'https://www.gxres.net'
+      rel: "canonical",
+      href: "https://www.gxres.net",
     },
     {
-      rel: 'preconnect',
-      href: 'https://library.gxres.net'
+      rel: "preconnect",
+      href: "https://library.gxres.net",
     },
     {
-      rel: 'icon',
-      href: 'https://library.gxres.net/images/icons/favicon.webp'
-    }
-  ]
-})
+      rel: "icon",
+      href: "https://library.gxres.net/images/icons/favicon.webp",
+    },
+  ],
+});
 
 useSeoMeta({
   title: `${profileStore.name} (@${profileStore.id})`,
   ogTitle: `${profileStore.name} (@${profileStore.id})`,
   description: `Introduction of ${profileStore.name}`,
   ogDescription: `Introduction of ${profileStore.name}`,
-  ogImage: `https://library.gxres.net/images/icons/favicon.webp`
-})
+  ogImage: `https://library.gxres.net/images/icons/favicon.webp`,
+});
 </script>
 
 <template>
+
   <Head>
     <Title>{{ profileStore.name }} (@{{ profileStore.id }})</Title>
   </Head>
-  <div class="p-4">
-    <div class="max-w-screen-md container mx-auto">
-      <div class="card">
-        <div class="container mx-auto p-4">
-          <div class="grid justify-center items-center space-y-3">
-            <div class="flex justify-center">
-              <NuxtImg preload placeholder width="150" height="150" class="rounded-full" @click="toggleContent" :src="profileStore.imageUrl" :alt="`${profileStore.name}'s avatar'`" />
-            </div>
-            <div class="text-center">
-              <div class="text-3xl">{{ profileStore.name }}</div>
-              <div class="text-sm">a.k.a. {{ profileStore.akaName }}</div>
-            </div>
+  <div class="max-w-2xl container mx-auto min-h-screen">
+    <div class="grid grid-cols-1 place-items-center text-center">
+      <div class="p-6 w-full">
+        <div class="box content">
+          <div>
+            <NuxtImg class="rounded-full" width="128" height="128" @click="toggleContent" :src="profileStore.imageUrl"
+              :alt="`${profileStore.name}'s avatar'`" />
           </div>
-        </div>
-        <div class="tabs is-centered">
+          <h1>{{ profileStore.name }}</h1>
+          <p>@{{ profileStore.id }} · a.k.a. {{ profileStore.akaName }}</p>
+          <div class="buttons flex justify-center">
+            <a class="button" href="https://blog.gxres.net">
+              <span class="icon i-ic-baseline-book" />
+              <span>Blog</span>
+            </a>
+            <a class="button" href="https://library.gxres.net">
+              <span class="icon i-ic-baseline-library-books" />
+              <span>Library</span>
+            </a>
+          </div>
+          <h2>Self introduce</h2>
+          <p>I am a student who comes from Guangxi, China.</p>
+          <p>I am also a noob on the Internet.</p>
+          <h2>Organizations</h2>
+          <p>
+            <a href="https://github.com/s-complex" target="_blank">Sliver Complex</a>
+            ·
+            <a href="https://github.com/LittleSkinCommspt" target="_blank">LittleSkin Community</a>
+            ·
+            <a href="https://speed.14cloud.cn" target="_blank">14Cloud</a>
+            Management Team
+          </p>
+          <h2>Skills</h2>
+          <div class="tags flex justify-center">
+            <span class="tag">Vue</span>
+            <span class="tag">OpenWRT</span>
+            <span class="tag">Linux</span>
+            <span class="tag">Home Network</span>
+            <span class="tag">Python</span>
+            <span class="tag">Photograph</span>
+          </div>
+          <blockquote>
+            <p>I haven't mastered any skills, although I'm always trying.</p>
+          </blockquote>
+          <h2>Favorite</h2>
           <ul>
             <li>
-              <a href="https://blog.gxres.net" aria-label="To Restent's Notebook">
-                <span class="icon is-small">
-                  <div class="i-ic-baseline-book" />
-                </span>
-                <span>Blog</span>
-              </a>
+              Playing Minecraft and rhythm games (prefer Phigros & Arcaea);
             </li>
-            <li>
-              <a href="https://library.gxres.net" aria-label="To SliverRiver's Library">
-                <span class="icon is-small">
-                  <div class="i-ic-baseline-library-books" />
-                </span>
-                <span>Library</span>
-              </a>
-            </li>
+            <li>Eating delicious food;</li>
+            <li>Go outside with my friends I guess.</li>
           </ul>
         </div>
-        <div class="card-content">
-          <div class="content">
-            <h1>👋 Hey</h1>
-            This is {{ profileStore.name }}, just a noob on the Internet. :P
-            <h2>I am...</h2>
-            <ul>
-              <li>Senior high student from Guangxi, China;</li>
-              <li>Member of <a href="https://github.com/LittleSkinCommspt"
-                  aria-label="Link to LittleSkin Community">LittleSkin Community</a>;</li>
-              <li>Contributor of <a href="https://go.14cloud.cn" aria-label="Link to 14Cloud">14Cloud</a> and BlissROM
-                (device: phoenix).</li>
-              <li>Maybe more?</li>
-            </ul>
-            <h2>My skills</h2>
-            <p>With the passion of front-end, I like to create front-end projects with Vue. Actually, I had learned
-              React before, but it's too hard for me to learn haha.</p>
-            <p>Besides, I've learned other skills that are necessary for my daily routine, although I haven't mastered
-              one of them till now.</p>
-            <h2>My favorite</h2>
-            <p>I am a fan of rhythm games and Minecraft, but now I usually spend my gaming time on rhythm games.</p>
-            <p>Also, it's hard for me to reject delicious foods!</p>
-            <h2>Find me at...</h2>
-            <div class="buttons">
-              <a class="button" :href="`https://github.com/${profileStore.id}`" aria-label="Visit my GitHub">
-                <div class="icon i-mdi-github" />
-                <div>GitHub</div>
-              </a>
-              <a class="button" href="https://m.flyemoji.moe/@slivermoe" aria-label="Visit my mastodon">
-                <div class="icon i-mdi-mastodon" />
-                <div>Mastodon</div>
-              </a>
-              <a class="button" href="https://library.gxres.net/contact.html" aria-label="More ways to find me">
-                <div class="icon i-ic-baseline-more-horiz" />
-                <div>More</div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="text-center text-sm">
-        Copyright © 2019 - {{ new Date().getFullYear() }} {{ profileStore.name }}. All rights reserved.
       </div>
     </div>
   </div>
