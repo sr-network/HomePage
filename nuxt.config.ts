b9929554a122fc6d5a1c0193119b88f7414f8d87
gxres042 <i@gxres.net>
2024-07-26 19:20:20 +0800

    Remove umami

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,16 +5,6 @@ export default defineNuxtConfig({
   experimental: {
     componentIslands: true,
   },
-  appConfig: {
-    umami: {
-      host: "https://umami.slirv.vip/",
-      id: "2d998227-18d9-4bb5-bbce-bb1e54c56fe5",
-      autoTrack: true,
-      version: 2,
-      useDirective: true,
-      debug: true,
-    },
-  },
   srcDir: "./src",
   modules: [
     "@unocss/nuxt",
