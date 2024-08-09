355d6f394bc047ca7210d4dde5e4bb28abc03f10
gxres042 <i@gxres.net>
2024-08-09 14:49:39 +0800

    Modify config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,13 +29,4 @@ export default defineNuxtConfig({
     classSuffix: "",
     storageKey: "nuxt-color-mode",
   },
-  appConfig: {
-    umami: {
-      host: "https://umami.slirv.vip/",
-      id: "fbe8ac0c-641d-4d0a-a03e-1e98844c9c52",
-      autoTrack: true,
-      ignoreLocalhost: true,
-      version: 2
-    }
-  }
 });
---
+++ src/app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  umami: {
+    host: "https://umami.slirv.vip/",
+    id: "fbe8ac0c-641d-4d0a-a03e-1e98844c9c52",
+    autoTrack: true,
+    ignoreLocalhost: true,
+    version: 2,
+  },
+});
